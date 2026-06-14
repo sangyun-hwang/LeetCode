52c00c6d708300639bf496428da87b071481ba99
SangYun Hwang <s_infinite@naver.com>
2026-06-14 22:46:17 +0900

    Time: 7 ms (38.04%), Space: 19.4 MB (23.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -6,11 +6,10 @@ class Solution:
         for i in range(len(s)):
             if s[i] in s_to_t and s_to_t[s[i]] != t[i]:
                 return False
-            else:
-                if t[i] in t_to_s and t_to_s[t[i]] != s[i]:
-                    return False
-                else:
-                    s_to_t[s[i]] = t[i]
-                    t_to_s[t[i]] = s[i]
-        
+            if t[i] in t_to_s and t_to_s[t[i]] != s[i]:
+                return False
+
+            s_to_t[s[i]] = t[i]
+            t_to_s[t[i]] = s[i]
+
         return True
